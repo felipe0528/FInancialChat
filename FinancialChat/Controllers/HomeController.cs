dd3670ac0f6791a1018f8ae1d75d4dd58e36464c
Juan Felipe Varner <felipe0528@gmail.com>
2020-04-10 11:23:25 -0500

    RabbitMQ Integration and View

--- FinancialChat/Controllers/HomeController.cs
+++ FinancialChat/Controllers/HomeController.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using FinancialChat.Helper;
+using Microsoft.AspNet.Identity;
+using RabbitMQ.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI.WebControls;
 
 namespace FinancialChat.Controllers
 {
@@ -11,7 +15,48 @@ namespace FinancialChat.Controllers
         [Authorize]
         public ActionResult Index()
         {
+            var currentUser = User.Identity.GetUserName();
+            List<ListItem> listItems = new List<ListItem>();
+            listItems.Add(new ListItem
+            {
+                Text = currentUser,
+                Value = currentUser
+            });
+            ViewBag.Users = listItems;
             return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public JsonResult sendmsg(string message, string recipient)
+        {
+            RabbitMQOperations rabbit = new RabbitMQOperations();
+            IConnection con = rabbit.GetConnection();
+            var currentUser = User.Identity.GetUserName();
+            message = currentUser + ": " + message;
+            bool flag = rabbit.send(con, message, recipient);
+            return Json(null);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public JsonResult receive()
+        {
+            try
+            {
+                RabbitMQOperations rabbit = new RabbitMQOperations();
+                IConnection con = rabbit.GetConnection();
+                var userqueue = User.Identity.GetUserName();
+                string message = rabbit.receive(con, userqueue);
+                return Json(message);
+            }
+            catch (Exception)
+            {
+
+                return null;
+            }
+
+
         }
 
         public ActionResult About()
